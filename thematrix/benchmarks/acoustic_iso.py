c33fef8435c7285529aea273301963c0823b26ac
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-04-29 10:38:58 +0200

    Custom caching for acoustic_iso

--- thematrix/benchmarks/acoustic_iso.py
+++ thematrix/benchmarks/acoustic_iso.py
@@ -1,4 +1,8 @@
-from devito import switchconfig
+import os
+from tempfile import gettempdir
+
+from devito import switchconfig, __version__ as devito_version
+from devito.operator.profiling import PerfEntry
 from examples.seismic.acoustic.acoustic_example import acoustic_setup
 
 from thematrix.benchmarks.common import check_norms
@@ -22,22 +26,36 @@ class IsotropicAcoustic(object):
 
     @switchconfig(profiling='advanced')
     def setup(self, shape, space_order, norms):
-        solver = acoustic_setup(shape=shape, space_order=space_order, tn=self.tn,
-                                opt=('advanced', {'openmp': True}))
-        rec, u, self.summary = solver.forward(x0_blk0_size=self.x0_blk0_size,
-                                              y0_blk0_size=self.y0_blk0_size)
+        filename = 'acoustic_iso_shape%s_so%d_devito%s.asv' % (str(shape).replace(" ", ""),
+                                                               space_order,
+                                                               devito_version.split('.')[1])
+        filename = os.path.join(gettempdir(), filename)
 
-        # Compare output against reference norms
-        check_norms([rec, u], norms)
+        try:
+            with open(filename, 'r') as f:
+                self.summary = eval(f.read())
+        except FileNotFoundError:
+            solver = acoustic_setup(shape=shape, space_order=space_order, tn=self.tn,
+                                    opt=('advanced', {'openmp': True}))
+            rec, u, summary = solver.forward(x0_blk0_size=self.x0_blk0_size,
+                                             y0_blk0_size=self.y0_blk0_size)
+            self.summary = summary.globals['fdlike']
+
+            # Compare output against reference norms
+            check_norms([rec, u], norms)
+
+            # Custom caching -- ASV's setup_cache won't work
+            with open(filename, 'w') as f:
+                f.write(str(self.summary))
 
     def track_runtime(self, shape, space_order, norms):
-        return self.summary.globals['fdlike'].time
+        return self.summary.time
     track_runtime.unit = "runtime"
 
     def track_gflopss(self, shape, space_order, norms):
-        return self.summary.globals['fdlike'].gflopss
+        return self.summary.gflopss
     track_gflopss.unit = "gflopss"
 
     def track_gpointss(self, shape, space_order, norms):
-        return self.summary.globals['fdlike'].gpointss
+        return self.summary.gpointss
     track_gpointss.unit = "gpointss"
