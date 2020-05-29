c43824f95d09f6dd2c5af7aefc0c48b903ae1252
rhodrin <rhodri.nelson@gmail.com>
2020-05-29 11:03:22 +0100

    Add elastic benchmark.

---
+++ thematrix/elastic.py
@@ -0,0 +1,40 @@
+from devito.operator.profiling import PerfEntry
+from examples.seismic.elastic.elastic_example import elastic_setup
+
+from thematrix.common import check_norms, run_prepare, run_benchmark
+
+
+class Elastic(object):
+
+    # Problem setup
+    params = ([(350, 350, 350)], [12], [{'rec1': 6.6363115, 'rec2': 0.71874803}])
+    param_names = ['shape', 'space_order', 'norms']
+    tn = 100
+
+    # ASV parameters
+    repeat = 1
+    timeout = 900.0
+    processes = 1
+
+    def setup(self, shape, space_order, norms):
+        fn_perf, fn_norms = run_prepare('elastic', shape, space_order)
+        try:
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
+        except FileNotFoundError:
+            run_benchmark('elastic', shape, space_order, self.tn, fn_perf, fn_norms)
+            check_norms(fn_norms, norms)
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
+
+    def track_runtime(self, shape, space_order, norms):
+        return self.summary.time
+    track_runtime.unit = "runtime"
+
+    def track_gflopss(self, shape, space_order, norms):
+        return self.summary.gflopss
+    track_gflopss.unit = "gflopss"
+
+    def track_gpointss(self, shape, space_order, norms):
+        return self.summary.gpointss
+    track_gpointss.unit = "gpointss"
