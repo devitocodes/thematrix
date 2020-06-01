eb5f72034d7259cb4c53d0d086ab3265f01c7d3d
mloubout <mathias.louboutin@gmail.com>
2020-06-01 12:22:41 -0400

    add all operators to benchmark

--- thematrix/acoustic_tti.py
+++ thematrix/acoustic_tti.py
@@ -4,7 +4,7 @@ from examples.seismic.tti.tti_example import tti_setup
 from thematrix.common import check_norms, run_prepare, run_benchmark
 
 
-class TTIAcoustic(object):
+class TTIAcousticForward(object):
 
     # Problem setup
     params = ([(350, 350, 350)], [12], [{'rec': 66.417102, 'u': 30.707737, 'v': 30.707728}])
@@ -38,3 +38,40 @@ class TTIAcoustic(object):
     def track_gpointss(self, shape, space_order, norms):
         return self.summary.gpointss
     track_gpointss.unit = "gpointss"
+
+
+class TTIAcousticAdjoint(object):
+
+    # Problem setup
+    params = ([(350, 350, 350)], [12], [{'srca': 184.526400, 'v': 151.545837}])
+    param_names = ['shape', 'space_order', 'norms']
+    tn = 100
+
+    # ASV parameters
+    repeat = 1
+    timeout = 900.0
+    processes = 1
+
+    def setup(self, shape, space_order, norms):
+        fn_perf, fn_norms = run_prepare('acoustic-ttia', shape, space_order)
+        try:
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
+        except FileNotFoundError:
+            run_benchmark('tti', shape, space_order, self.tn, fn_perf, fn_norms, op="adjoint")
+            check_norms(fn_norms, norms)
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
+
+    def track_runtime(self, shape, space_order, norms):
+        return self.summary.time
+    track_runtime.unit = "runtime"
+
+    def track_gflopss(self, shape, space_order, norms):
+        return self.summary.gflopss
+    track_gflopss.unit = "gflopss"
+
+    def track_gpointss(self, shape, space_order, norms):
+        return self.summary.gpointss
+    track_gpointss.unit = "gpointss"
+
