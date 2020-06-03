b59f4d63d6be4a5de2b7043837a7446cc8ddeb59
rhodrin <rhodri.nelson@gmail.com>
2020-06-03 18:03:51 +0100

    Re-add more benchmarks (Devito patch should mean they now work correctly).

--- thematrix/acoustic_tti.py
+++ thematrix/acoustic_tti.py
@@ -40,38 +40,38 @@ class TTIAcousticForward(object):
     track_gpointss.unit = "gpointss"
 
 
-#class TTIAcousticAdjoint(object):
+class TTIAcousticAdjoint(object):
 
-    ## Problem setup
-    #params = ([(350, 350, 350)], [12], [{'srca': 184.526400, 'v': 151.545837}])
-    #param_names = ['shape', 'space_order', 'norms']
-    #tn = 100
+    # Problem setup
+    params = ([(350, 350, 350)], [12], [{'srca': 184.526400, 'v': 151.545837}])
+    param_names = ['shape', 'space_order', 'norms']
+    tn = 100
 
-    ## ASV parameters
-    #repeat = 1
-    #timeout = 900.0
-    #processes = 1
+    # ASV parameters
+    repeat = 1
+    timeout = 900.0
+    processes = 1
 
-    #def setup(self, shape, space_order, norms):
-        #fn_perf, fn_norms = run_prepare('acoustic-ttia', shape, space_order)
-        #try:
-            #with open(fn_perf, 'r') as f:
-                #self.summary = eval(f.read())
-        #except FileNotFoundError:
-            #run_benchmark('tti', shape, space_order, self.tn, fn_perf, fn_norms, op="adjoint")
-            #check_norms(fn_norms, norms)
-            #with open(fn_perf, 'r') as f:
-                #self.summary = eval(f.read())
+    def setup(self, shape, space_order, norms):
+        fn_perf, fn_norms = run_prepare('acoustic-ttia', shape, space_order)
+        try:
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
+        except FileNotFoundError:
+            run_benchmark('tti', shape, space_order, self.tn, fn_perf, fn_norms, op="adjoint")
+            check_norms(fn_norms, norms)
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
 
-    #def track_runtime(self, shape, space_order, norms):
-        #return self.summary.time
-    #track_runtime.unit = "runtime"
+    def track_runtime(self, shape, space_order, norms):
+        return self.summary.time
+    track_runtime.unit = "runtime"
 
-    #def track_gflopss(self, shape, space_order, norms):
-        #return self.summary.gflopss
-    #track_gflopss.unit = "gflopss"
+    def track_gflopss(self, shape, space_order, norms):
+        return self.summary.gflopss
+    track_gflopss.unit = "gflopss"
 
-    #def track_gpointss(self, shape, space_order, norms):
-        #return self.summary.gpointss
-    #track_gpointss.unit = "gpointss"
+    def track_gpointss(self, shape, space_order, norms):
+        return self.summary.gpointss
+    track_gpointss.unit = "gpointss"
 
