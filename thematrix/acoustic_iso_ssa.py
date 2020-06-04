de813b3d7c5adafafdcffc9ed6603cc7ea6c4b79
rhodrin <rhodri.nelson@gmail.com>
2020-06-04 10:29:07 +0100

    Temp. remove some jobs to try and get actions working again.

--- thematrix/acoustic_iso_ssa.py
+++ thematrix/acoustic_iso_ssa.py
@@ -4,145 +4,145 @@ from examples.seismic.acoustic.acoustic_example import acoustic_setup
 from thematrix.common import check_norms, run_prepare, run_benchmark
 
 
-#class IsotropicSSAForward(object):
+class IsotropicSSAForward(object):
 
-    ## Problem setup
-    #params = ([(152, 152, 152)], [12], [{'rec': 247.956100, 'u': 7438246.500000}])
-    #param_names = ['shape', 'space_order', 'norms']
-    #tn = 100
+    # Problem setup
+    params = ([(152, 152, 152)], [12], [{'rec': 247.956100, 'u': 7438246.500000}])
+    param_names = ['shape', 'space_order', 'norms']
+    tn = 100
 
-    ## ASV parameters
-    #repeat = 1
-    #timeout = 900.0
-    #processes = 1
+    # ASV parameters
+    repeat = 1
+    timeout = 900.0
+    processes = 1
 
-    #def setup(self, shape, space_order, norms):
-        #fn_perf, fn_norms = run_prepare('acoustic-ssa-isof', shape, space_order)
-        #try:
-            #with open(fn_perf, 'r') as f:
-                #self.summary = eval(f.read())
-        #except FileNotFoundError:
-            #run_benchmark('acoustic_ssa', shape, space_order, self.tn, fn_perf, fn_norms)
-            #check_norms(fn_norms, norms)
-            #with open(fn_perf, 'r') as f:
-                #self.summary = eval(f.read())
+    def setup(self, shape, space_order, norms):
+        fn_perf, fn_norms = run_prepare('acoustic-ssa-isof', shape, space_order)
+        try:
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
+        except FileNotFoundError:
+            run_benchmark('acoustic_ssa', shape, space_order, self.tn, fn_perf, fn_norms)
+            check_norms(fn_norms, norms)
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
 
-    #def track_runtime(self, shape, space_order, norms):
-        #return self.summary.time
-    #track_runtime.unit = "runtime"
+    def track_runtime(self, shape, space_order, norms):
+        return self.summary.time
+    track_runtime.unit = "runtime"
 
-    #def track_gflopss(self, shape, space_order, norms):
-        #return self.summary.gflopss
-    #track_gflopss.unit = "gflopss"
+    def track_gflopss(self, shape, space_order, norms):
+        return self.summary.gflopss
+    track_gflopss.unit = "gflopss"
 
-    #def track_gpointss(self, shape, space_order, norms):
-        #return self.summary.gpointss
-    #track_gpointss.unit = "gpointss"
-
-
-#class IsotropicSSAAdjoint(object):
-
-    ## Problem setup
-    #params = ([(152, 152, 152)], [12], [{'srca': 390.499268, 'v': 157214277632.000000}])
-    #param_names = ['shape', 'space_order', 'norms']
-    #tn = 100
-
-    ## ASV parameters
-    #repeat = 1
-    #timeout = 900.0
-    #processes = 1
-
-    #def setup(self, shape, space_order, norms):
-        #fn_perf, fn_norms = run_prepare('acoustic-ssa-isoa', shape, space_order)
-        #try:
-            #with open(fn_perf, 'r') as f:
-                #self.summary = eval(f.read())
-        #except FileNotFoundError:
-            #run_benchmark('acoustic_ssa', shape, space_order, self.tn, fn_perf, fn_norms, op="adjoint")
-            #check_norms(fn_norms, norms)
-            #with open(fn_perf, 'r') as f:
-                #self.summary = eval(f.read())
-
-    #def track_runtime(self, shape, space_order, norms):
-        #return self.summary.time
-    #track_runtime.unit = "runtime"
-
-    #def track_gflopss(self, shape, space_order, norms):
-        #return self.summary.gflopss
-    #track_gflopss.unit = "gflopss"
-
-    #def track_gpointss(self, shape, space_order, norms):
-        #return self.summary.gpointss
-    #track_gpointss.unit = "gpointss"
+    def track_gpointss(self, shape, space_order, norms):
+        return self.summary.gpointss
+    track_gpointss.unit = "gpointss"
 
 
-#class IsotropicSSAJacobian(object):
+class IsotropicSSAAdjoint(object):
 
-    ## Problem setup
-    #params = ([(152, 152, 152)], [12], [{'rec': 0.070716, 'u0': 7438246.000000, 'du': 18011874.000000}])
-    #param_names = ['shape', 'space_order', 'norms']
-    #tn = 100
+    # Problem setup
+    params = ([(152, 152, 152)], [12], [{'srca': 390.499268, 'v': 157214277632.000000}])
+    param_names = ['shape', 'space_order', 'norms']
+    tn = 100
 
-    ## ASV parameters
-    #repeat = 1
-    #timeout = 900.0
-    #processes = 1
+    # ASV parameters
+    repeat = 1
+    timeout = 900.0
+    processes = 1
 
-    #def setup(self, shape, space_order, norms):
-        #fn_perf, fn_norms = run_prepare('acoustic-ssa-isoJ', shape, space_order)
-        #try:
-            #with open(fn_perf, 'r') as f:
-                #self.summary = eval(f.read())
-        #except FileNotFoundError:
-            #run_benchmark('acoustic_ssa', shape, space_order, self.tn, fn_perf, fn_norms, op="jacobian")
-            #check_norms(fn_norms, norms)
-            #with open(fn_perf, 'r') as f:
-                #self.summary = eval(f.read())
+    def setup(self, shape, space_order, norms):
+        fn_perf, fn_norms = run_prepare('acoustic-ssa-isoa', shape, space_order)
+        try:
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
+        except FileNotFoundError:
+            run_benchmark('acoustic_ssa', shape, space_order, self.tn, fn_perf, fn_norms, op="adjoint")
+            check_norms(fn_norms, norms)
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
 
-    #def track_runtime(self, shape, space_order, norms):
-        #return self.summary.time
-    #track_runtime.unit = "runtime"
+    def track_runtime(self, shape, space_order, norms):
+        return self.summary.time
+    track_runtime.unit = "runtime"
 
-    #def track_gflopss(self, shape, space_order, norms):
-        #return self.summary.gflopss
-    #track_gflopss.unit = "gflopss"
+    def track_gflopss(self, shape, space_order, norms):
+        return self.summary.gflopss
+    track_gflopss.unit = "gflopss"
 
-    #def track_gpointss(self, shape, space_order, norms):
-        #return self.summary.gpointss
-    #track_gpointss.unit = "gpointss"
+    def track_gpointss(self, shape, space_order, norms):
+        return self.summary.gpointss
+    track_gpointss.unit = "gpointss"
 
 
-#class IsotropicSSAJacobianAdjoint(object):
+class IsotropicSSAJacobian(object):
 
-    ## Problem setup
-    #params = ([(152, 152, 152)], [12], [{'dm': 16766.755859, 'u': 7628539.000000, 'du': 3355461376.000000}])
-    #param_names = ['shape', 'space_order', 'norms']
-    #tn = 100
+    # Problem setup
+    params = ([(152, 152, 152)], [12], [{'rec': 0.070716, 'u0': 7438246.000000, 'du': 18011874.000000}])
+    param_names = ['shape', 'space_order', 'norms']
+    tn = 100
 
-    ## ASV parameters
-    #repeat = 1
-    #timeout = 900.0
-    #processes = 1
+    # ASV parameters
+    repeat = 1
+    timeout = 900.0
+    processes = 1
 
-    #def setup(self, shape, space_order, norms):
-        #fn_perf, fn_norms = run_prepare('acoustic-ssa-isoJa', shape, space_order)
-        #try:
-            #with open(fn_perf, 'r') as f:
-                #self.summary = eval(f.read())
-        #except FileNotFoundError:
-            #run_benchmark('acoustic_ssa', shape, space_order, self.tn, fn_perf, fn_norms, op="jacobian_adjoint")
-            #check_norms(fn_norms, norms)
-            #with open(fn_perf, 'r') as f:
-                #self.summary = eval(f.read())
+    def setup(self, shape, space_order, norms):
+        fn_perf, fn_norms = run_prepare('acoustic-ssa-isoJ', shape, space_order)
+        try:
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
+        except FileNotFoundError:
+            run_benchmark('acoustic_ssa', shape, space_order, self.tn, fn_perf, fn_norms, op="jacobian")
+            check_norms(fn_norms, norms)
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
 
-    #def track_runtime(self, shape, space_order, norms):
-        #return self.summary.time
-    #track_runtime.unit = "runtime"
+    def track_runtime(self, shape, space_order, norms):
+        return self.summary.time
+    track_runtime.unit = "runtime"
 
-    #def track_gflopss(self, shape, space_order, norms):
-        #return self.summary.gflopss
-    #track_gflopss.unit = "gflopss"
+    def track_gflopss(self, shape, space_order, norms):
+        return self.summary.gflopss
+    track_gflopss.unit = "gflopss"
 
-    #def track_gpointss(self, shape, space_order, norms):
-        #return self.summary.gpointss
-    #track_gpointss.unit = "gpointss"
+    def track_gpointss(self, shape, space_order, norms):
+        return self.summary.gpointss
+    track_gpointss.unit = "gpointss"
+
+
+class IsotropicSSAJacobianAdjoint(object):
+
+    # Problem setup
+    params = ([(152, 152, 152)], [12], [{'dm': 16766.755859, 'u': 7628539.000000, 'du': 3355461376.000000}])
+    param_names = ['shape', 'space_order', 'norms']
+    tn = 100
+
+    # ASV parameters
+    repeat = 1
+    timeout = 900.0
+    processes = 1
+
+    def setup(self, shape, space_order, norms):
+        fn_perf, fn_norms = run_prepare('acoustic-ssa-isoJa', shape, space_order)
+        try:
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
+        except FileNotFoundError:
+            run_benchmark('acoustic_ssa', shape, space_order, self.tn, fn_perf, fn_norms, op="jacobian_adjoint")
+            check_norms(fn_norms, norms)
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
+
+    def track_runtime(self, shape, space_order, norms):
+        return self.summary.time
+    track_runtime.unit = "runtime"
+
+    def track_gflopss(self, shape, space_order, norms):
+        return self.summary.gflopss
+    track_gflopss.unit = "gflopss"
+
+    def track_gpointss(self, shape, space_order, norms):
+        return self.summary.gpointss
+    track_gpointss.unit = "gpointss"
