82a6353245c21b2e4ba39f9045d19cce686f07f0
rhodrin <rhodri.nelson@gmail.com>
2020-06-04 13:11:42 +0100

    Re-introduce one more SSA benchmark.

--- thematrix/acoustic_iso_ssa.py
+++ thematrix/acoustic_iso_ssa.py
@@ -40,40 +40,40 @@ class IsotropicSSAForward(object):
     track_gpointss.unit = "gpointss"
 
 
-#class IsotropicSSAAdjoint(object):
+class IsotropicSSAAdjoint(object):
 
-    ## Problem setup
-    #params = ([(152, 152, 152)], [12], [{'srca': 390.499268, 'v': 157214277632.000000}])
-    #param_names = ['shape', 'space_order', 'norms']
-    #tn = 100
+    # Problem setup
+    params = ([(152, 152, 152)], [12], [{'srca': 390.499268, 'v': 157214277632.000000}])
+    param_names = ['shape', 'space_order', 'norms']
+    tn = 100
 
-    ## ASV parameters
-    #repeat = 1
-    #timeout = 900.0
-    #processes = 1
+    # ASV parameters
+    repeat = 1
+    timeout = 900.0
+    processes = 1
 
-    #def setup(self, shape, space_order, norms):
-        #fn_perf, fn_norms = run_prepare('acoustic-ssa-isoa', shape, space_order)
-        #try:
-            #with open(fn_perf, 'r') as f:
-                #self.summary = eval(f.read())
-        #except FileNotFoundError:
-            #run_benchmark('acoustic_ssa', shape, space_order, self.tn, fn_perf, fn_norms, op="adjoint")
-            #check_norms(fn_norms, norms)
-            #with open(fn_perf, 'r') as f:
-                #self.summary = eval(f.read())
+    def setup(self, shape, space_order, norms):
+        fn_perf, fn_norms = run_prepare('acoustic-ssa-isoa', shape, space_order)
+        try:
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
+        except FileNotFoundError:
+            run_benchmark('acoustic_ssa', shape, space_order, self.tn, fn_perf, fn_norms, op="adjoint")
+            check_norms(fn_norms, norms)
+            with open(fn_perf, 'r') as f:
+                self.summary = eval(f.read())
 
-    #def track_runtime(self, shape, space_order, norms):
-        #return self.summary.time
-    #track_runtime.unit = "runtime"
+    def track_runtime(self, shape, space_order, norms):
+        return self.summary.time
+    track_runtime.unit = "runtime"
 
-    #def track_gflopss(self, shape, space_order, norms):
-        #return self.summary.gflopss
-    #track_gflopss.unit = "gflopss"
+    def track_gflopss(self, shape, space_order, norms):
+        return self.summary.gflopss
+    track_gflopss.unit = "gflopss"
 
-    #def track_gpointss(self, shape, space_order, norms):
-        #return self.summary.gpointss
-    #track_gpointss.unit = "gpointss"
+    def track_gpointss(self, shape, space_order, norms):
+        return self.summary.gpointss
+    track_gpointss.unit = "gpointss"
 
 
 #class IsotropicSSAJacobian(object):
