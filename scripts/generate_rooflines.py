eeb89d7690cacd62abfc5c2a99ce21861ca9af10
jack-lascala <gl818@ic.ac.uk>
2020-09-03 08:54:53 +0000

    generate_rooflines: Automatic benchmark importing

--- scripts/generate_rooflines.py
+++ scripts/generate_rooflines.py
@@ -1,29 +1,37 @@
+from importlib import import_module
 import inspect
 from pathlib import Path
+from pkgutil import iter_modules
+
 import thematrix
-import thematrix.acoustic_iso
-import thematrix.acoustic_iso_adj
-import thematrix.acoustic_iso_jac
-import thematrix.acoustic_iso_jacadj
-import thematrix.acoustic_iso_ssa
-import thematrix.acoustic_iso_ssa_adj
-import thematrix.acoustic_iso_ssa_jacadj
-import thematrix.acoustic_tti
-import thematrix.acoustic_tti_adj
-import thematrix.elastic
-import thematrix.viscoelastic
 
 
 def find_all_roofline_methods():
+    """
+    Testing implementation: discover all rooflining functions
+
+    """
+
+    _import_thematrix_submodules()
     for name, data in inspect.getmembers(thematrix):
-      if name.startswith('__'):
-        continue
-      print('Looking at: %s' % name)
-      for subname, subdata in inspect.getmembers(data, predicate=inspect.isclass):
-        if not subname == 'PerfEntry':
-          for function_name, _ in inspect.getmembers(subdata, predicate=inspect.isfunction):
-            print(function_name)
-    
+        print(name)
+
+
+def _import_thematrix_submodules():
+    """
+    Import all the modules defined in package thematrix to
+    be able to perform inspection.
+
+    """
+
+    for _, name, ispkg in iter_modules(thematrix.__path__):
+        if not ispkg:
+            module_name = thematrix.__name__ + '.' + name
+            try:
+                import_module(module_name)
+            except ImportError:
+                pass
+
 
 if __name__ == '__main__':
     find_all_roofline_methods()
