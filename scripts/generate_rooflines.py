f3efc73416e1a6aaf27157bd0c5a6ea860c0d649
jack-lascala <gl818@ic.ac.uk>
2020-09-17 11:10:27 +0000

    generate_rooflines: Collection of directories generated in file

--- scripts/generate_rooflines.py
+++ scripts/generate_rooflines.py
@@ -2,7 +2,9 @@ from importlib import import_module
 import inspect
 from pathlib import Path
 from pkgutil import iter_modules
+from tempfile import gettempdir
 from traceback import format_exc
+import os
 
 import thematrix
 
@@ -13,10 +15,19 @@ def generate_rooflines():
     generate all rooflines for all problems
 
     """
+    # Set up the options
+    opts = {'advisor': True}
+    generated_dirs = []
 
     roof_methods = _find_roofline_methods()
     for (method, cls) in roof_methods:
-        method(cls)
+        # Run the method and retrieve the results directory
+        generated_dir = method(cls, opts)
+        if generated_dir:
+            generated_dirs.append(generated_dir)
+
+    # Package the retrieved result directories into a file
+    _write_generated_dirs(generated_dirs)
 
 
 def _find_roofline_methods():
@@ -33,7 +44,7 @@ def _find_roofline_methods():
         # Collect the roofline methods from the classes
         for c_name, c_data in inspect.getmembers(data, predicate=inspect.isclass):
             for m_name, m_data in inspect.getmembers(c_data, predicate=inspect.isfunction):
-                if m_name.startswith('roofline'):
+                if m_name == 'roofline_create':
                     roof_methods.append((m_data, c_data))
 
     return roof_methods
@@ -54,6 +65,17 @@ def _import_thematrix_submodules():
             except ImportError:
                 print('Error: could not import module ' + module_name)
                 print(format_exc())
+
+
+def _write_generated_dirs(generated_dirs):
+    gen_file_dir = os.path.join(gettempdir(), 'generate_rooflines_tmp')
+    if not os.path.isdir(gen_file_dir):
+        os.mkdir(gen_file_dir)
+
+    gen_file_path = os.path.join(gen_file_dir, 'generated.txt')
+    with open(gen_file_path, 'w+') as f:
+        f.writelines(generated_dirs)
+    print('Rooflines successfully generated, directories listed in: ' + gen_file_path)
 
 
 if __name__ == '__main__':
